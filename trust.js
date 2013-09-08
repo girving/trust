3c6478e8f99d8f2b460773692608be3e829473b1
Geoffrey Irving <irving@naml.us>
2013-09-08 15:40:13 -0700

    Draw the graph directed

--- trust.js
+++ trust.js
@@ -41,18 +41,19 @@ function Graph() {
   //     4 - valid
   //     5 - signature by key owner expired
   //     6 - signature by key owner revoked
-  function Sig (id,issuer,code,link) {
-    this.id = id
+  function Sig (node,issuer,code,link) {
+    this.node = node
     this.issuer = issuer
     this.code = code
     this.link = link
-    this.toString = function () { return show_id(this.id)+'-'+show_id(this.issuer)+'-'+this.code }
+    this.toString = function () { return show_id(this.node.id)+'-'+show_id(this.issuer.id)+'-'+this.code }
   }
   function Link (source,target) {
     this.source = source
     this.target = target
   }
   this.links = [] // Ordered list of link objects
+  this.all_sigs = [] // Ordered list of all sig objects
   this.sigs = {} // a -> b -> link if b signs a
   this.inv_sigs = {} // a -> b -> true if a in sigs[b]
 
@@ -75,15 +76,14 @@ function Graph() {
 
   // b signs a with given code
   this.add_edge = function (a,b,code) {
-    this.add_node(a)
-    this.add_node(b)
+    var an = this.add_node(a)
+    var bn = this.add_node(b)
     if (!(b in this.sigs[a])) {
-      var ai = this.id_to_node[a].index
-      var bi = this.id_to_node[b].index
       var link = a in this.sigs[b] ? this.sigs[b][a].link
-                                   : new Link(ai,bi)
-      var sig = new Sig(a,b,code,link)
-      if (link.source == ai)
+                                   : new Link(an,bn)
+      var sig = new Sig(an,bn,code,link)
+      this.all_sigs.push(sig)
+      if (link.source === an)
         this.links.push(link)
       this.sigs[a][b] = sig
       this.inv_sigs[b][a] = true
@@ -172,19 +172,6 @@ function update_key(graph,id) {
   }
 }
 
-function link_color (graph,link) {
-  var a = link.source.id
-  var b = link.target.id
-  var ab = b in graph.sigs[a] ? graph.sigs[a][b].code : -1
-  var ba = a in graph.sigs[b] ? graph.sigs[b][a].code : -1
-  if (ab==4 && ba==4)
-    return 'green'
-  if (   (ab==2 || ab==4 || ab==-1)
-      && (ba==2 || ba==4 || ba==-1))
-    return 'orange'
-  return 'red'
-}
-
 // Toplevel
 window.onload = function () {
   openpgp.init()
@@ -192,24 +179,70 @@ window.onload = function () {
   var width = 640
   var height = 480
   var radius = 10
+  var bend = .07
   var svg = d3.select('body').append('svg')
     .attr('width',width)
     .attr('height',height)
+  svg.append('defs').selectAll('marker')
+      .data(['valid','invalid','pending'])
+    .enter().append('marker')
+      .attr('id',String)
+      .attr('orient','auto')
+      .attr('markerWidth',10)
+      .attr('markerHeight',10)
+      .attr('refX',2)
+      .attr('refY',2)
+      .append('polygon')
+        .attr('points','4,2 0,4 0,0')
   var force = d3.layout.force()
     .linkDistance(100)
     .size([width,height])
+
+  var sig_colors = {valid:'green',invalid:'red',pending:'orange'}
+  function sig_status (sig) {
+    if (sig.code==4) return 'valid'
+    if (sig.code==2) return 'pending'
+    return 'invalid'
+  }
+
+  function sig_path (sig) {
+    var x0 = sig.issuer.x, 
+        y0 = sig.issuer.y,
+        x4 = sig.node.x,
+        y4 = sig.node.y
+    if (sig.node.id in graph.sigs[sig.issuer.id]) { // Flipped edge exists, use an arc
+      // We subdivide once so that the midpoint exists for arrow purposes
+      var xc = .5*(x0+x4)+bend*(y4-y0),
+          yc = .5*(y0+y4)-bend*(x4-x0),
+          x1 = .5*(x0+xc),
+          y1 = .5*(y0+yc),
+          x3 = .5*(x4+xc),
+          y3 = .5*(y4+yc),
+          x2 = .5*(x1+x3),
+          y2 = .5*(y1+y3)
+      return 'M'+x0+','+y0
+           +' Q'+x1+','+y1
+           + ' '+x2+','+y2
+           +' T'+x4+','+y4 
+    }
+    var x2 = .5*(x0+x4),
+        y2 = .5*(y0+y4)
+    return 'M'+x0+','+y0
+         +' L'+x2+','+y2
+         + ' '+x4+','+y4
+  }
 
   graph.onchange = function () {
     force.nodes(graph.nodes)
     force.links(graph.links)
     force.start()
 
-    var link = svg.selectAll('.link')
-      .data(graph.links)
-    link.enter().insert('line','circle')
-      .attr('class','link')
-
-    link.style('stroke', function (d) { return link_color(graph,d) })
+    var sig = svg.selectAll('.sig')
+      .data(graph.all_sigs)
+    sig.enter().insert('path','.node')
+      .attr('class','sig')
+    sig.style('stroke', function (s) { return sig_colors[sig_status(s)] })
+      .attr('marker-mid', function (s) { return 'url(#'+sig_status(s)+')' })
 
     var node = svg.selectAll('.node')
       .data(graph.nodes)
@@ -224,10 +257,7 @@ window.onload = function () {
       .text(function (d) { return graph.long_name(d.id) })
 
     force.on('tick', function () {
-      link.attr('x1', function (d) { return d.source.x })
-          .attr('y1', function (d) { return d.source.y })
-          .attr('x2', function (d) { return d.target.x })
-          .attr('y2', function (d) { return d.target.y })
+      sig.attr('d',sig_path)
       node.attr('cx', function (d) { return d.x })
           .attr('cy', function (d) { return d.y })
     })
